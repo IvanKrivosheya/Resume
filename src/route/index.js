1f162ca29d0690e30fd66ce95f7bc15c3c884046
IvanKrivosheya <128534291+IvanKrivosheya@users.noreply.github.com>
2023-05-18 12:09:58 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,26 +5,293 @@ const router = express.Router()
 
 // ================================================================
 
+var header ={
+	name: {
+		firstname: 'Ivan',
+		lastname: 'Krivosheya',
+		},
+	
+	position:'Junior Fullstack JS Developer',
+	
+	salary: {
+		cost:'600$',
+		text:'в місяць',
+		},
+	address: 'Ukraine',
+};
+
+var footer = {
+	social: {
+		email: {
+			text: 'ivanov@mail.com', 
+			href: 'mailto:ivanov@mail.com', 
+			},  
+	
+
+		phone: {
+			text: '+380688943294',
+			href: 'tel:+380688943294',
+			},
+		LinkedIn: {
+			href: 'https://www.linkedin.com/in/ivan-krivosheya-baa58b274/',
+			text: 'LinkedIn',
+			},
+	},
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
+// res.render генерує нам HTML сторінку
 
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
-  //                  ↑↑ сюди вводимо JSON дані
+//            ↙ cюди вводимо назву файлу з сontainer
+res.render('index', {})
+//                  ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
 
 //              ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/summary', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('summary', {
-    // ↙ сюди вводимо JSON дані
-  })
+//             ↙ cюди вводимо назву файлу з сontainer
+res.render('summary', {
+	// ↙ сюди вводимо JSON дані
+
+	page: {
+		title: 'Resume | Summary',
+	},
+
+	header,
+
+	main: {
+			summary: {
+				title: 'Summary',
+				big: true,
+				text: `Open-minded for new technologies, with 1 years of
+				experience in development. Whenever I start to
+				work on a new project I learn the domain and try
+				to understand the idea of the project. Good team
+				player, every colleague is a friend to me.`,
+			},
+		
+			experience: { 
+				title:'Other experience', 
+				big: false,
+				text:`Pet project
+				for parsing sport betting data from different platforms (
+				odds ) and sport statistics ( tournament position, goals
+				etc), analyzing by simple mathematics models and preparing
+				probability for such events like: money line - first win /
+				draw / second win, totals etc.`,
+			},
+		},
+
+	footer,
 })
+})
+
+
+router.get('/skills', function (req, res) {
+//             ↙ cюди вводимо назву файлу з сontainer
+res.render('skills', {
+	// ↙ сюди вводимо JSON дані
+
+	page: {
+		title: 'Resume | Skills',
+	},
+
+	header,
+
+	main: {
+		skills: [
+			{ 
+				name: "HTML",
+				point: 10,
+				isTop: true,
+			},
+			{ 
+				name: "Hendlbars",
+				point: 9,
+				isTop: false,
+
+			},
+			{ 
+				name: "VS Code",
+				point: 10,
+				isTop: true,
+			},
+			{ 
+				name: "Git",
+				point: 10,
+				isTop: true,
+			},
+			{ 
+				name: "Terminal",
+				point: 6,
+				isTop:false,
+			},
+			{ 
+				name: "NPM",
+				point: 5,
+				isTop: false,
+			},
+			{
+				name: "React.js",
+				point: 0,
+			},
+			{
+				name: "PHP",
+				point: null,
+			},
+		],
+		
+		hobbies: [
+			{
+				name:"doing sports",
+				isMain: true
+			},
+			{
+				name:"riding a bike",
+				isMain: true
+			},
+			{
+				name: "playing the guitar",
+				isMain: false
+			},
+		],
+	},
+	
+	footer,
+	
+})
+})
+
+
+
+router.get('/education', function (req, res) {
+	//             ↙ cюди вводимо назву файлу з сontainer
+	res.render('education', {
+	// ↙ сюди вводимо JSON дані
+
+	page: {
+		title: 'Resume | Educatin',
+	},
+
+	header,
+
+	main: {
+		educations: [
+			{
+				name: "Державний податковий Університет",
+				end: 2023,
+				isEnd: true,
+			},
+			{
+				name: "Національна академія внутрішніх справ",
+				end: 2022,
+				isEnd: true,
+			},
+			{
+				name: "Law schoolss",
+				end: null,
+				isEnd: false,
+			},
+		],
+
+		certificates: [
+			{
+				name: "Державний подаковий Університет",
+				id: 123456
+			},
+			{
+				name: "НАВС",
+				id: 456789
+			},
+			{
+				name: " Law schools",
+				id: 789123
+			},
+		],
+	},
+	
+	footer,
+	
+	})
+})
+
+
+
+router.get('/work', function (req, res) {
+	//             ↙ cюди вводимо назву файлу з сontainer
+	res.render('work', {
+		// ↙ сюди вводимо JSON дані
+
+		layout: 'big',
+	
+		page: {
+			title: 'Resume | Work',
+		},
+	
+		
+	
+		main: {
+			works: [
+				{
+					position: 'Junior Fullstack Developer',
+					company: {
+						name: 'IT Brains',
+						url: null,
+					},
+					duration: {
+						from: "10.10.2020",
+						to: null,
+					},
+					projectAmount: "3", 
+					projects: [
+						{
+							name: 'Resume',
+							url: 'https://web.skillzrun.com/learn/123771',
+							about: 'Some info',
+							
+							stackAmount: '3',
+							stacks: [
+								{
+									name: 'React.js',
+								},
+								{
+									name: 'HTML / CSS',
+								},
+								{
+									name: 'Node.js',
+								},
+							],
+							awardAmount: '2',
+							awards: [
+								{
+									name: 'Beckground verification...',
+								},
+								{
+									name: 'Prepearing SEO optimized pages...',
+								},
+							],
+						},
+					],
+					
+					
+				},
+			],
+		},
+
+				
+	
+		footer,
+	})
+	})
+	
+
 
 // ================================================================
 
